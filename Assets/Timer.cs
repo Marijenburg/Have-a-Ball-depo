ed5d07e37fd1d68818f15dadd3776f715843bd99
DesktopMarijenburg <cdesaintjores@outlook.com>
2020-07-01 19:26:51 +0200

    Adding Timer, half way

---
+++ Assets/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart ;
+    public TextMeshPro textBox;
+
+    bool timerActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+       
+            timeStart += Time.deltaTime;
+            textBox.text = Mathf.Round(timeStart).ToString("F2");
+       
+    }
+}
