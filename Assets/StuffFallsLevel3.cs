b2e723bb4aea00946bddbba1779be2cb60720242
DesktopMarijenburg <cdesaintjores@outlook.com>
2020-07-06 11:51:19 +0200

    work on animation of gates

---
+++ Assets/StuffFallsLevel3.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuffFallsLevel3 : MonoBehaviour
+{
+    public GameObject obstacle;
+    private bool isTriggered = false;
+
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (isTriggered == false)
+
+            Debug.Log("touch the FTHING");
+        Instantiate(obstacle, new Vector3(0, 40, 600), Quaternion.identity);
+
+        isTriggered = true;
+
+    }
+
+
+
+
+}
