701309a20d19924f07e49a23f0a594d19e1c8cd7
DesktopMarijenburg <cdesaintjores@outlook.com>
2020-07-16 04:06:59 +0200

    -Cube Destructible is now an effect particles that auto destroy -Start messing around with timer and ready script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
     public AudioSource rollingSound;
     public AudioSource landingSound;
     public AudioSource powerUp;
-    public GameObject landingEffect;
+    
 
 
     void FixedUpdate()
@@ -53,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
             {
 
                 landingSound.Play();
-                Instantiate(landingEffect, rb.transform.position,Quaternion.Euler(90f, 0f,0f) );
+                
                 Debug.Log("sound landing PLAY");
             }
         }
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -8,18 +8,31 @@ public class Timer : MonoBehaviour
 {
     public float timeStart ;
     public Text textBox;
+    
 
     // Start is called before the first frame update
-   
-
-    // Update is called once per frame
-    void Update()
+    public void TimerStart()
     {
-
         timeStart += Time.deltaTime;
         print(timeStart);
         textBox.text = timeStart.ToString("0.00");
     }
+
+    public void TimerStops()
+    {
+
+    }
+    public void SetTimerToZero()
+    {
+        textBox.text = "0";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        TimerStart();
+       
+    }
+   
 
 }
 
